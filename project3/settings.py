09f95e09f5fdd79262a16c93f62677fd16322812
Himaloy Mondal <himaloymondal100@gmail.com>
2021-05-04 20:22:13 +0530

    Fix static files error

--- project3/settings.py
+++ project3/settings.py
@@ -120,7 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    'mail/static'
-]
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
