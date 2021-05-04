ea24dbe68d943154598a7c37538a4ce43d14db78
Himaloy Mondal <himaloymondal100@gmail.com>
2021-05-04 19:54:28 +0530

    Fix staticfiles problem

--- project3/settings.py
+++ project3/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'mail' / 'static'
+]
